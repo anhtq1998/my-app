54c7ce8e2640bebae7b46e467018fb7fad279cb5
20167063 <47656743+20167063@users.noreply.github.com>
2022-04-19 15:09:04 +0700

    add file

---
+++ src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { registerLocaleData } from '@angular/common';
+import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
+import en from '@angular/common/locales/en';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { en_US, NZ_I18N } from 'ng-zorro-antd/i18n';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IconsProviderModule } from './icons-provider.module';
+import { HeaderComponent } from './pages/header/header.component';
+
+
+import { DemoNgZorroAntdModule } from './ng-zorro-antd.module';
+
+
+
+
+registerLocaleData(en);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule,
+    FormsModule,
+    HttpClientJsonpModule,
+    ReactiveFormsModule,
+    ScrollingModule,
+    DragDropModule,
+    DemoNgZorroAntdModule
+  ],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
